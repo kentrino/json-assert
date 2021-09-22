a0bdf6f5d07f91387b70f8d6149e835b8b9ed0cd
kentrino <kentrino@h-kento.jp>
2021-09-22 13:26:20 +0900

    :hammer: (lib/build.gradle.kts) fix dependency

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -20,23 +20,14 @@ repositories {
 }
 
 dependencies {
-    // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("io.github.portfoligno:jackson-immutable-ast:1.4.1")
+    api("io.github.portfoligno:jackson-immutable-ast:1.4.1")
 
     val jacksonVersion = "2.12.5"
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation("com.google.guava:guava:29.0-jre")
-
-    // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-    // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 
     testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
